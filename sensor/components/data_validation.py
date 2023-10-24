33317473f40f935eae7a126c28d4373a8a8ea0f9
Mohammed Mujtaba Hussain <mujtabamohammed.h123@gmail.com>
2023-10-24 20:17:21 +0530

    data transformation completed

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -124,10 +124,12 @@ class DataValidation:
                 validation_status=status,
                 valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
                 valid_test_file_path=self.data_ingestion_artifact.test_file_path,
-                invalid_train_file_path=self.data_ingestion_artifact.trained_file_path,
-                invalid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
                 drift_report_file_path=self.data_validation_config.drift_report_file_path,
             )
+            logging.info(f"data validation artifact: {data_validation_artifact}")
+            return data_validation_artifact
  
             
             
